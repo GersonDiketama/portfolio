bb1dbf215b691c01f0a22893b060df414fa670e0
Gerson Diketama <gersondiketama@Gersons-MacBook-Pro.local>
2021-08-31 15:30:30 -0500

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+var timeLine = new TimelineMax();
+
+timeLine
+.from('.apple-icon',4,{ opacity:1 }).to('.apple-icon',3,{ opacity:0 })
+
+.from('.text',1,{opacity:1}).to('.text',1,{opacity:0})
+
+.from('.text-1',1,{opacity:1}).to('.text-1',1,{opacity:0})
+
+.from('.text-2',1,{opacity:1}).to('.text-2',1,{opacity:0})
+
+
+.from('.text-3',1,{opacity:0}).to('.text-3',2,{opacity:1})
